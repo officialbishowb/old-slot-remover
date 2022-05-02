0c6cfca0a4f61ff3781d836bdf0e9259eb120b96
Bishow Bhattarai <bbhattarai@student.tgm.ac.at>
2022-05-02 08:03:16 +0200

    rows are displayed none instead of empty

--- src/oldslot_remover.js
+++ src/oldslot_remover.js
@@ -1,59 +1,85 @@
 // ==UserScript==
-// @name         New Slot Remover
+// @name         Old slot remover
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  This script removes the old/expired slots that you can see if you want to register a time slot for
-// @author       Edited by Riedler
-// @match        https://elearning.tgm.ac.at/mod/scheduler/view.php?*
+// @description This script removes the old/expired slots that you can see if you want to register a time slot for [MEDT,NWTK,SYT,ITSI,SEW]
+// @author       @officialbishowb[github.com/officialbishowb]
+// @match        https://elearning.tgm.ac.at/mod/scheduler/view.php?id=219076
+// @match        https://elearning.tgm.ac.at/mod/scheduler/view.php?id=219078
+// @match        https://elearning.tgm.ac.at/mod/scheduler/view.php?id=219079
+// @match        https://elearning.tgm.ac.at/mod/scheduler/view.php?id=225390
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ac.at
 // @grant        none
-// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-    //get current timestamp in seconds
-    let now = Math.floor(Date.now() / 1000);
-    //get all tables in main card (presumably all are timetables (this might change & break things horribly (😳 (cry about it))))
-    for (let table of document.getElementById("region-main").getElementsByClassName("generaltable")) {
-        for (let datetag of table.getElementsByClassName("datelabel")) {
-            let time = getTimeFromDate(datetag.textContent);
-            if (time < now) { //if time lies in the past,
-                //make row invisible because deleting it makes elearning freak out
-                datetag.parentElement.parentElement.style.display = "none";
-            } else {
-                console.log(time);
-                console.log(now);
-            }
+
+    let getTable = document.getElementsByTagName("table")[0];
+    let getRows = getTable.getElementsByTagName("tr");
+
+    let currentDate = new Date().setHours(0, 0, 0, 0) / 1000;
+
+
+    var toRemove = 0;
+    var totalCounts = getRows.length - 1;
+    for (let i = 0; i < getRows.length; i++) {
+        let currentData = getRows[i].getElementsByTagName("td")[0];
+        console.log(currentData);
+        if (currentData.className == "header c5 lastcol") continue;
+
+        let date = currentData.firstChild.innerText;
+
+        // Since some of the date values are not valid and therefore cannot be converted to a Date object, why not create one?
+        // Beside that the format for Date is same for every subject
+        date = getValidDate(date).getTime() / 1000;
+
+        if (date < currentDate) toRemove++;
+
+
+
+    }
+
+    if (toRemove >= totalCounts) {
+        // remove the whole table as it contains only expired slots
+        getTable.style.display = "none";
+    } else {
+        // Remove the table rows from the top
+        for (let i = 0; i < totalCounts; i++) {
+            getRows[i].style.display = "none";
         }
     }
+
+
+
 })();
 
-function getTimeFromDate(string_date) {
-    const months = {
-        "jänner": "JAN",
-        "januar": "JAN",
-        "februar": "FEB",
-        "märz": "MAR",
-        "april": "APR",
-        "mai": "MAY",
-        "juni": "JUN",
-        "juli": "JUL",
-        "august": "AUG",
-        "september": "SEP",
-        "oktober": "OKT",
-        "november": "NOV",
-        "dezember": "DEC"
-    };
-    //"Freitag, 11. März 2022" → {"11.","März","2022"}
-    let _date = string_date.split(",")[1].split(" ");
-    //"11." → "11"
-    let day = _date[1].replace(".", "");
-    //"März" → "MAR"
-    let month = months[_date[2].replace(" ", "").toLowerCase()];
-    //year is just year :)
-    let year = _date[3];
 
-    //parse again \m/ and return as timestamp in seconds
-    return Date.parse(month + " " + day + " " + year) / 1000;
+function getValidDate(string_date) {
+    const months = {
+        "MAR": "März",
+        "MAY": "Mai",
+        "JUN": "Juni",
+        "JUL": "Juli",
+        "OCT": "Oktober",
+        "DEC": "Dezember"
+    };
+    let dates = string_date.split(",")[1].split(" ");
+    let day = dates[1].split(".")[0].replace(" ", "");
+    let realMonth = dates[2].replace(" ", "");
+    let year = dates[3].replace(" ", "");
+    // get valid month from the const months
+
+    let newDate = new Date(realMonth + " " + day + " " + year)
+
+    if (newDate != "Invalid Date") return newDate;
+
+    // If it is Invalid Date then probably month is not convertable
+
+    for (let m in months) {
+        if (months[m] == realMonth) realMonth = m;
+    }
+
+    // If the error still occurs then I dont know the fix :(
+    return new Date(realMonth + " " + day + " " + year);
 }
